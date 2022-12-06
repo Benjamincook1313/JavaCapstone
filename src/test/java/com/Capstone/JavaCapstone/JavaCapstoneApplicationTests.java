95fae8617f91d269a9765a4fd5113da8df76f91c
Benjamin Cook <benjamincook1313@gmail.com>
2022-12-05 17:51:12 -0700

    register

--- src/test/java/com/Capstone/JavaCapstone/JavaCapstoneApplicationTests.java
+++ src/test/java/com/Capstone/JavaCapstone/JavaCapstoneApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaCapstoneApplicationTests {
+public class JavaCapstoneApplicationTests {
 
 	@Autowired
   UserRepo userRepo;
@@ -20,7 +20,7 @@ class JavaCapstoneApplicationTests {
 	ListRepo listRepo;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 	User user1 = new User();
@@ -50,58 +50,58 @@ class JavaCapstoneApplicationTests {
 		userRepo.save(user2);
 	}
 
-	@Test
-	public void createLists() {
-		list1.setTitle("Snowboard");
-		list1.setType(ListTypes.valueOf("ITEM"));
-		list1.setOwner(user1);
+//	@Test
+//	public void createLists() {
+//		list1.setTitle("Snowboard");
+//		list1.setType(ListTypes.valueOf("ITEM"));
+//		list1.setOwner(user1);
+//
+//		list2.setTitle("Surf/Beach");
+//		list2.setType(ListTypes.valueOf("ITEM"));
+//		list2.setOwner(user1);
+//
+//		list3.setTitle("Fly-Fish");
+//		list3.setType(ListTypes.valueOf("ITEM"));
+//		list3.setOwner(user1);
+//
+//		list4.setTitle("Costco");
+//		list4.setType(ListTypes.valueOf("SHOP"));
+//		list4.setOwner(user2);
+//
+//		list5.setTitle("Sprouts");
+//		list5.setType(ListTypes.valueOf("SHOP"));
+//		list5.setOwner(user2);
+//
+//		listRepo.save(list1);
+//		listRepo.save(list2);
+//		listRepo.save(list3);
+//		listRepo.save(list4);
+//		listRepo.save(list5);
+//	}
 
-		list2.setTitle("Surf/Beach");
-		list2.setType(ListTypes.valueOf("ITEM"));
-		list2.setOwner(user1);
-
-		list3.setTitle("Fly-Fish");
-		list3.setType(ListTypes.valueOf("ITEM"));
-		list3.setOwner(user1);
-
-		list4.setTitle("Costco");
-		list4.setType(ListTypes.valueOf("SHOP"));
-		list4.setOwner(user2);
-
-		list5.setTitle("Sprouts");
-		list5.setType(ListTypes.valueOf("SHOP"));
-		list5.setOwner(user2);
-
-		listRepo.save(list1);
-		listRepo.save(list2);
-		listRepo.save(list3);
-		listRepo.save(list4);
-		listRepo.save(list5);
-	}
-
-	@Test
-	public void addListItems(){
-		Item item1 = new Item();
-		item1.setList(list1);
-		item1.setName("snowboard");
-
-		Item item2 = new Item();
-		item2.setName("goggles");
-		item2.setList(list1);
-
-		Item item3 = new Item();
-		item3.setName("helmet");
-		item3.setList(list1);
-
-		Item item4 = new Item();
-		item4.setName("surfboard");
-		item4.setList(list2);
-		Item item5 = new Item();
-		item5.setName("rod");
-		item5.setList(list3);
-		Item item6 = new Item();
-		item6.setName("protein shakes");
-		item6.setList(list4);
-	}
+//	@Test
+//	public void addListItems(){
+//		Item item1 = new Item();
+//		item1.setList(list1);
+//		item1.setName("snowboard");
+//
+//		Item item2 = new Item();
+//		item2.setName("goggles");
+//		item2.setList(list1);
+//
+//		Item item3 = new Item();
+//		item3.setName("helmet");
+//		item3.setList(list1);
+//
+//		Item item4 = new Item();
+//		item4.setName("surfboard");
+//		item4.setList(list2);
+//		Item item5 = new Item();
+//		item5.setName("rod");
+//		item5.setList(list3);
+//		Item item6 = new Item();
+//		item6.setName("protein shakes");
+//		item6.setList(list4);
+//	}
 
 }
